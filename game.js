30af1af37c616b0802ab1aed4869c9dca632f726
Mayank Goel <goel.mayank235@gmail.com>
2022-04-20 17:45:34 +0530

    Added level 7

--- game.js
+++ game.js
@@ -3,23 +3,44 @@ var gamePattern = [];
 var userClickedPattern = [];
 var randomNumber;
 var randomChosenColour;
+var level = 0;
+var started = false
 
+$(document).keypress(function () {
+    if (!started) {
+        nextSequence();
+        started = true;
+    }
+});
+
+$("[type=button]").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    nextSequence();
+});
+
+var playSound = (name) => {
+    var audio = new Audio("sounds/" + name + ".mp3 ");
+    audio.play();
+}
 var nextSequence = () => {
 
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttoncolors[randomNumber];
     gamePattern.push(randomChosenColour);
-    var audio = new Audio("sounds\ " + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
+    $("h1").text("Level " + level);
+    level++;
+
+    $("#" + randomChosenColour).fadeTo(100, 0.3, function () {
+        $(this).fadeTo(500, 1.0);
+    });
 }
-
-
-$("#" + randomChosenColour).fadeTo(100, 0.3, function () {
-    $(this).fadeTo(500, 1.0);
-});
-
-
-$("[type=button]").click(function () {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-});
+var animatePress = (chosenColor) => {
+    $("#" + chosenColor).addClass("pressed");
+    setTimeout(() => {
+        $("#" + chosenColor).removeClass("pressed");
+    }, 100)
+}
