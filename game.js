ff1bd2f535bdb5118945b5e1ec8c24eeb50c9dae
Mayank Goel <goel.mayank235@gmail.com>
2022-04-20 17:03:40 +0530

    Added 4th part

--- game.js
+++ game.js
@@ -1,17 +1,25 @@
 var buttoncolors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 var randomNumber;
 var randomChosenColour;
+
 var nextSequence = () => {
 
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttoncolors[randomNumber];
     gamePattern.push(randomChosenColour);
+    var audio = new Audio("sounds\ " + randomChosenColour + ".mp3");
+    audio.play();
 }
+
 
 $("#" + randomChosenColour).fadeTo(100, 0.3, function () {
     $(this).fadeTo(500, 1.0);
 });
 
-var audio = new Audio("sounds\ " + randomChosenColour + ".mp3");
-audio.play();
+
+$("[type=button]").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+});
